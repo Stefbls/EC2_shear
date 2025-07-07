3f3f496579eeb17f429fc6536f97d3f0c6539519
Stefbls <stefano.bilosi@gmail.com>
2025-07-07 13:41:37 +0200

    Update EC2_shear.py

--- EC2_shear.py
+++ EC2_shear.py
@@ -8,8 +8,8 @@ def main():
     st.header("Verifica di Snellezza di un Pilastro in CLS")
     st.header("Inserisci i dati di input")
     
-    b = st.number_input("Larghezza trave (b) [cm]", min_value=0.0, step=0.01, value=2.0)
-    h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.01, value=2.0)
+    b = st.number_input("Larghezza trave (b) [cm]", min_value=0.0, step=0.1, value=30.0)
+    h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=30.0)
     A = b * h
     st.latex(r" A = b x h ")
     st.latex(f" A = {b:.1f} cm × {h:.1f} cm  = {A:.1f} cm^2")
