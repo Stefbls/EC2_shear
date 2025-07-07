5d7a16573bb4ef3b107d18757a778c570057c7b3
Stefbls <stefano.bilosi@gmail.com>
2025-07-07 17:18:51 +0200

    Update EC2_shear.py

--- EC2_shear.py
+++ EC2_shear.py
@@ -9,7 +9,7 @@ def main():
     st.header("Inserisci i dati di input")
     
     b = st.number_input("Larghezza trave (b) [cm]", min_value=0.0, step=0.1, value=30.0)
-    h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=30.0)
+    h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=40.0)
     A = b * h
     col1, col2 = st.columns(2)       
     with col1:
@@ -20,11 +20,11 @@ def main():
         st.latex(f"A = {b:.1f} \\, \\text{{cm}} \\, \\times \\, {h:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
     
     c = st.number_input("Copriferro (c) [cm]", min_value=0.0, step=0.1, value=3.0)
-    n_barre = st.number_input("N° barre (n_barre) [-]", min_value=0.0, step=0.1, value=3.0)
-    HA = st.number_input("Diametro barre (HA) [-]", min_value=0.0, step=0.1, value=3.0)
-    A_s = n_barre * 3.14 * HA**2 / 4
-    st.latex(r"A_s = n_{barre} \times HA^2 ")
-    st.latex(f"A_s = {n_barre} \\,  \\, \\times \\, {HA:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
+    n_barre = st.number_input("Numero di barre tese (n_barre) [-]", min_value=0.0, step=0.1, value=3.0)
+    HA = st.number_input("Diametro barre (HA) [mm]", min_value=0.0, step=0.1, value=3.0)
+    A_s = n_barre * (math.pi * HA**2) / (4 * 100)
+    st.latex(r"A_s = n_{barre} \cdot \frac{\pi \cdot \phi^2}{4 \cdot 100}")
+    st.latex(f"{n_barre} \\cdot \\frac{{\\pi \\cdot {HA:.1f}^2}}{{4 \\cdot 100}}")
 
 
 
