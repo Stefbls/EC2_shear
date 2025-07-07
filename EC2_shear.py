8e39a3d66b8a25d9245518c1692c73f0b7d5b618
Stefbls <stefano.bilosi@gmail.com>
2025-07-07 13:50:09 +0200

    Update EC2_shear.py

--- EC2_shear.py
+++ EC2_shear.py
@@ -5,15 +5,22 @@ import numpy as np
 
 def main():
     
-    st.header("Verifica di Snellezza di un Pilastro in CLS")
+    st.header("Verifica taglio trave")
     st.header("Inserisci i dati di input")
     
     b = st.number_input("Larghezza trave (b) [cm]", min_value=0.0, step=0.1, value=30.0)
     h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=30.0)
     A = b * h
-    st.latex(r"A = b \times h ")
-    st.latex(f"A = {b:.1f} \\, \\text{{cm}} \\, \\times \\, {h:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
 
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.markdown("**Formula simbolica**")
+        st.latex(r"A = b \times h ")
+
+    with col2:
+    st.markdown("**Formula numerica**")
+    st.latex(f"A = {b:.1f} \\, \\text{{cm}} \\, \\times \\, {h:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
 
 if __name__ == "__main__":
     main()
