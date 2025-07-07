5f9de2fa3d0843248255e6390b50d7f56f296249
Stefbls <stefano.bilosi@gmail.com>
2025-07-07 13:45:34 +0200

    Update EC2_shear.py

--- EC2_shear.py
+++ EC2_shear.py
@@ -11,10 +11,9 @@ def main():
     b = st.number_input("Larghezza trave (b) [cm]", min_value=0.0, step=0.1, value=30.0)
     h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=30.0)
     A = b * h
-    st.latex(r" A = b x h ")
-    st.latex(f" A = {b:.1f} cm × {h:.1f} cm  = {A:.1f} cm^2")
-    
-    
+    st.latex(r"A = b \times h ")
+    st.latex(f"A = {b:.1f} \\, \\text{{cm}} \\, \\times \\, {h:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
+
 
 if __name__ == "__main__":
     main()
