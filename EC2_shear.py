6be3d731d61403e18aadfb936fe5ddb4e956d2c3
Stefbls <stefano.bilosi@gmail.com>
2025-07-07 17:05:39 +0200

    Update EC2_shear.py

--- EC2_shear.py
+++ EC2_shear.py
@@ -12,13 +12,17 @@ def main():
     h = st.number_input("Altezza trave (h) [cm]", min_value=0.0, step=0.1, value=30.0)
     A = b * h
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     with col1:
+        st.markdown("**A**")
+        st.latex(r"Area ")
+        
+    with col2:
         st.markdown("**Formula simbolica**")
         st.latex(r"A = b \times h ")
 
-    with col2:
+    with col3:
         st.markdown("**Formula numerica**")
         st.latex(f"A = {b:.1f} \\, \\text{{cm}} \\, \\times \\, {h:.1f} \\, \\text{{cm}} = {A:.1f} \\, \\text{{cm}}^2")
 
